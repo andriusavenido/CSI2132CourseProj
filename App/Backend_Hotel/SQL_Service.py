e622e13531939c6d992f9598ae9b58320b4260c7
Andrius <andriusavenido@gmail.com>
2026-03-27 17:18:18 -0400

    fix env

--- App/Backend_Hotel/SQL_Service.py
+++ App/Backend_Hotel/SQL_Service.py
@@ -2,6 +2,10 @@ import asyncpg
 import os
 from dotenv import load_dotenv
 
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+
+from logging import getLogger
+logger = getLogger(__name__)
 pool: asyncpg.pool.Pool = None
 
 """
@@ -9,11 +13,23 @@ Initialize the database connection pool using asyncpg.
 """
 async def init_db_pool():
     global pool
-    pool = await asyncpg.create_pool(
-        dsn=os.getenv("DATABASE_URL"),
-        min_size=1,
-        max_size=10
-    )
+    try:
+        db_url = os.getenv("DATABASE_URL")
+        if not db_url:
+            raise ValueError("DATABASE_URL is not set")
+
+        pool = await asyncpg.create_pool(
+            dsn=db_url,
+            min_size=1,
+            max_size=10
+        )
+
+        logger.info("Database pool initialized successfully")
+
+    except Exception as e:
+        logger.error(f"Failed to initialize DB pool: {e}")
+        raise
+
 
 async def insert_customer(customer):
     query = """
