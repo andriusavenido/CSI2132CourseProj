78e65b160982d045e1b0cb7ce81576a26d2f56fd
andriusavenido <andriusavenido@gmail.com>
2026-04-06 17:12:00 -0400

    trigger the trigger

---
+++ SQL Codes/triggerthetrigger.sql
@@ -0,0 +1,22 @@
+INSERT INTO Booking (
+    booking_id,
+    customer_id,
+    customer_name,
+    room_number,
+    hotel_chain,
+    hotel_id,
+	hotel,
+    price,
+    status
+)
+VALUES (
+    33,
+    1,
+    'Alice Johnson',
+    '101',
+    'GrandStay Hotels',
+	1,
+	'Hotel 1',
+    100,
+    'Booked'
+);
